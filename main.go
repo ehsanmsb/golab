9fe72fac3771ff4f4a123ec4a84ae37a50fa1bed
Ehsan Mosayebi <ehsan.mosaieby@gmail.com>
2026-01-05 19:53:45 +0330

    add os args

--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
+
 	fmt.Println("Hello, I'm Golab")
+
+	args := os.Args
+	fmt.Println("Args List:")
+	for index, arg := range args {
+		if index != 0 {
+			fmt.Println(arg)
+		}
+	}
 }
