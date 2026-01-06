d6a69562268633a8c314e2c0727d2e5d92772fbd
Ehsan Mosayebi <ehsan.mosaieby@gmail.com>
2026-01-06 14:23:21 +0330

    feat: add two args in command

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # golab
+
+You can use below command:
+go run main.go --url https://dummyjson.com/ip
--- main.go
+++ main.go
@@ -2,6 +2,10 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"net/url"
 	"os"
 )
 
@@ -10,10 +14,30 @@ func main() {
 	fmt.Println("Hello, I'm Golab")
 
 	args := os.Args
-	fmt.Println("Args List:")
-	for index, arg := range args {
-		if index != 0 {
-			fmt.Println(arg)
-		}
+	if len(args) <= 2 {
+		log.Fatal("Your run command must have at least two argument")
 	}
+	if args[1] != "--url" {
+		log.Fatalf("App hasn't %s arg", args[1])
+	}
+	if _, err := url.ParseRequestURI(args[2]); err != nil {
+		log.Fatal("Your url address is not valid")
+	}
+
+	var resp *http.Response
+	var err error
+
+	resp, err = http.Get(args[2])
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("The url status is %d\n", resp.StatusCode)
+	fmt.Printf("The response is %v", string(body))
+
 }
